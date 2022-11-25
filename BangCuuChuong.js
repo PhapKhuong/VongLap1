745756b6f262adf0768025ca33d11f118a36b3c0
PhapKhuong <phapkhuong@gmail.com>
2022-11-26 02:33:17 +0700

    Sửa lại bài bảng cửu chương

--- BangCuuChuong.js
+++ BangCuuChuong.js
@@ -1,15 +1,12 @@
 function showBangCuuChuong()
-    {
-        document.write('<table>');
+    {   
+        let text = ""; //Nếu không gán giá trị rỗng ban đầu cho text thì text lấy giá trị undefined; khi cộng chuỗi ở hàm bên dưới sẽ hiển thị thêm undefined nữa
         for (i = 1; i <= 9 ; i++) {
-            document.write ('<tr>');
             for (j = 2; j <= 9; j++) {
-                document.write
-                    (
-                        '<td class="style1">' + j + 'x' + i + '=' + i * j + '</td>'
-                    )
+                if (j === 2) text += '<tr><td class="td1">' + j + 'x' + i + '=' + i * j + '</td>';
+                else if (j === 9) text += '<td class="td1">' + j + 'x' + i + '=' + i * j + '</td></tr>';
+                else text += '<td class="td1">' + j + 'x' + i + '=' + i * j + '</td>';
                 }
-            document.write ('</tr>');
             }
-        document.write('</table>');
+        document.getElementById('result').innerHTML = '<table class="table1">' + text + '</table>';
     }
